056827776c883fa1dde42d590d2b7c93b55bfb16
Artur Ziółkowski <aaartiii99@gmail.com>
2023-05-14 17:42:27 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_POSTS = gql`
+
+    query {
+      getPosts{
+        id
+        body
+        author
+      }
+    }
+
+`;
 
 function App() {
+  const { error, loading, data } = useQuery(GET_POSTS);
+
+  useEffect(() => {
+    console.log(loading, error, data);
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data ? data.getPosts.map((post: any) => {
+        return (
+        <div>
+          <p>{post.id}</p> 
+          <p>{post.author}</p>
+          <p>{post.body}</p>
+        </div>)
+      }): null }
     </div>
   );
 }
